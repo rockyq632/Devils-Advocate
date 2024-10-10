8c17f7ba0df951a112123feb5c1203dde910c208
rockyq632 <rockyq632@gmail.com>
2024-10-10 18:04:02 -0400

    Created some glow shaders

--- Scenes/Levels/DEBUG_BATTLE/debug_battle.gd
+++ Scenes/Levels/DEBUG_BATTLE/debug_battle.gd
@@ -4,7 +4,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	GSM.is_pc_movement_locked = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
