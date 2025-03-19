25a94d98930a4683714bde5955788a0c6593c508
rockyq632 <rockyq632@gmail.com>
2025-03-19 16:26:22 -0400

    Resturctured the way the state machine works
     State machine now works in a consecutive order with individually configurable timers

--- Scenes/Enemies/built_enemies/Test_Enemy/test_enemy_sm.gd
+++ Scenes/Enemies/built_enemies/Test_Enemy/test_enemy_sm.gd
@@ -1,9 +1,9 @@
 extends EnemyStateMachine
 
-var state_timer:Timer
+#var state_timer:Timer
 
 
-
+'''
 func _ready() -> void:
 	for i:EnemyState in state_list:
 		if(i == idle_state):
@@ -22,3 +22,4 @@ func _ready() -> void:
 func _state_change_timeout() -> void:
 	#var new_index:int = randi_range(2,3)
 	change_state(3)
+'''
--- Scenes/Enemies/enemy_systems/enemy_state_machine.gd
+++ Scenes/Enemies/enemy_systems/enemy_state_machine.gd
@@ -3,45 +3,46 @@ extends Node
 
 signal state_changed
 
-# List of Enemy states
-@export var state_list:Array[EnemyState]
 
 # Enemy reference
 @export var enm_body:Enemy
 
-# Current state will set the first state (Can be left blank)
-@export var curr_state:EnemyState
+
+@export_subgroup("State Information")
+# List of Enemy states in order of use
+@export var state_list:Array[EnemyState]
+
+# List of times to stay in each state
+@export var state_times:Array[float] = [0.0]
 
 # Idle state used to return to whenever another state is finsished
 @export var idle_state:EnemyState
 
 
 @export_subgroup("MONITOR ONLY")
+# Stores the state index currently in use (for network purposes)
 @export var curr_state_index:int = 0
-@export var idle_state_index:int = 0
+# Current state will set the first state (Can be left blank)
+@export var curr_state:EnemyState
+
+
+var sm_timer:Timer
 
 
 func _ready() -> void:
-	#if Idle state exists
-	if( idle_state ):
-		idle_state._enter_state()
-		
-		# Searches for the idle state in the state list to set the index
-		for i:int in range(0,state_list.size()):
-			if(state_list[i] == idle_state):
-				idle_state_index = i
-	
-	#if current state exists
-	if( curr_state ):
-		# Searches for the current state in the state list to set the index
-		for i:int in range(0,state_list.size()):
-			if(state_list[i] == curr_state):
-				curr_state_index = i
-		
 	# Everything past here Only works if is host
 	if(not is_multiplayer_authority()):
 		return
-
+	
+	# Set up the state machine timer
+	sm_timer = Timer.new()
+	sm_timer.autostart = false
+	sm_timer.one_shot = true
+	sm_timer.wait_time = state_times[0]
+	add_child(sm_timer)
+	sm_timer.timeout.connect(_inc_state)
+	sm_timer.start()
+	
 
 
 
@@ -50,24 +51,32 @@ var d_sum:float = 0
 func _process(_delta: float) -> void:
 	# Guarantees that the state is entered
 	# Also works on network peers without an RPC call
-	if( curr_state ):
-		if( not curr_state.state_entered ):
-			curr_state._enter_state()
-	
-	
 	# Everything past here only runs on host
 	if(not is_multiplayer_authority()):
 		return
+	
+	if( curr_state ):
+		if( not curr_state.state_entered ):
+			curr_state._enter_state()
+
+
+
+func _inc_state() -> void:
+	curr_state_index += 1
+	if(curr_state_index >= state_list.size()):
+		curr_state_index = 0
+	
+	change_state( curr_state_index )
+
 
 
 
 # Called when the node enters the scene tree for the first time.
 func change_state(new_state_ind:int) -> void:
 	# Everything past here only runs on host
-	'''
+
 	if(not is_multiplayer_authority()):
 		return
-	'''
 	
 	#Check that the state exists
 	if( new_state_ind >= state_list.size()  or  new_state_ind < 0 ):
@@ -81,11 +90,24 @@ func change_state(new_state_ind:int) -> void:
 	curr_state = state_list[new_state_ind]
 	curr_state_index = new_state_ind
 	
+	
+	# Set the timer up for the next state change
+	# If there is a set time limit for the state
+	if(state_times[new_state_ind] > 0):
+		sm_timer.wait_time = state_times[new_state_ind]
+		sm_timer.start(0.0)
+	# If there's no set time limit for the state
+	else:
+		if( not state_list[new_state_ind].state_finished.is_connected(_inc_state) ):
+			state_list[new_state_ind].state_finished.connect(_inc_state)
+	
+	
 	# Emit state changed signal
 	state_changed.emit(state_list[new_state_ind])
 
 
-
+'''
 # Returns to the IDLE state
 func _return_to_idle() -> void:
 	change_state(idle_state_index)
+'''
